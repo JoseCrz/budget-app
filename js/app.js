8abb3c4b633d9f873467691f45f21f5d60e4f2ab
Jose Cuevas <josecueram@gmail.com>
2020-01-05 21:02:58 -0600

    Created method in UIController to get data from the inputs

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
+// ? Budget Controller
 const budgetController = (function () {
     let x = 23
-
+    
     const add = function (a) {
         return x + a
     }
-
+    
     return {
         publicTest : function (b) {
             return add(b)
@@ -12,14 +13,33 @@ const budgetController = (function () {
     }
 })()
 
+// ? UI Controller
+// * Receives data from the UI and sends it to it.
 const uiController = (function () {
-
+    const DOMSelectors = {
+        inputType: '#input-type',
+        inputDescription: '#input-description',
+        inputAmount: '#input-amount'  
+    }
+    return {
+        getInputs: function () {
+            return {
+                type: document.querySelector(DOMSelectors.inputType).value, //either "income" or "expense"
+                description: document.querySelector(DOMSelectors.inputDescription).value,
+                amount: document.querySelector(DOMSelectors.inputAmount).value
+            }
+        }
+    }
 })()
 
+// ? Global App Controller
 const controller = (function (budgetCntlr, uiCntrlr) {
     
     const ctrlrAddItem = function () {
-        // TODO 1. Get the field input data
+        // 1. Get the field input data
+        const inputs = uiController.getInputs()
+        console.log(inputs)
+
         // TODO 2. Add the item to the budget controller
         // TODO 3. Add the item to the UI
         // TODO 4. Calculate the budget
@@ -35,5 +55,5 @@ const controller = (function (budgetCntlr, uiCntrlr) {
             ctrlrAddItem()
         }
     })
-    
+
 })(budgetController, uiController)
