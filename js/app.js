34c7168b0131d816a62394b376123683001abef7
Jose Cuevas <josecueram@gmail.com>
2020-01-05 20:23:10 -0600

    Added first event listeners

--- js/app.js
+++ js/app.js
@@ -17,11 +17,23 @@ const uiController = (function () {
 })()
 
 const controller = (function (budgetCntlr, uiCntrlr) {
-    const z = budgetCntlr.publicTest(10)
-
-    return {
-        anotherPublicMethod : function () {
-            console.log(z)
-        }
+    
+    const ctrlrAddItem = function () {
+        // TODO 1. Get the field input data
+        // TODO 2. Add the item to the budget controller
+        // TODO 3. Add the item to the UI
+        // TODO 4. Calculate the budget
+        // TODO 5. Display the budgey in the UI
+        
+        console.log('It works!')
     }
+
+    document.querySelector('#button-add').addEventListener('click', ctrlrAddItem)
+    
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13) {
+            ctrlrAddItem()
+        }
+    })
+    
 })(budgetController, uiController)
